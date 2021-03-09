6525412a8eb0eae10c1c5fab7f25926c52a00cdd
erikaboth <erika.both@gmail.com>
2021-03-09 20:49:53 -0300

    Primeira implementação de ADC baseado em exemplo (IDF)

--- main/main.cpp
+++ main/main.cpp
@@ -20,7 +20,9 @@
 
 #include "Sleep.h"
 #include "TheThingsNetwork.h"
+#include "driver/adc.h"
 #include "driver/gpio.h"
+#include "esp_adc_cal.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
@@ -52,6 +54,13 @@ const char *appKey = "C0A9C01E65BB1B3FAB14360971CCBD3C";
 #define TTN_PIN_RST      14
 #define TTN_PIN_DIO0     26
 #define TTN_PIN_DIO1     35
+#define DEFAULT_VREF     1100  //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES    64    //Multisampling
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc1_channel_t channel = ADC1_CHANNEL_6;  //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
+static const adc_unit_t unit = ADC_UNIT_1;
 
 static TheThingsNetwork ttn;
 
@@ -135,6 +144,45 @@ static void SPI_init()
     ESP_ERROR_CHECK(spi_bus_initialize(TTN_SPI_HOST, &spi_bus_config, TTN_SPI_DMA_CHAN));
 }
 
+static void check_efuse()
+{
+    //Check TP is burned into eFuse
+    if(esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK)
+    {
+        printf("eFuse Two Point: Supported\n");
+    }
+    else
+    {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+
+    //Check Vref is burned into eFuse
+    if(esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK)
+    {
+        printf("eFuse Vref: Supported\n");
+    }
+    else
+    {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if(val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+    {
+        printf("Characterized using Two Point Value\n");
+    }
+    else if(val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
+    {
+        printf("Characterized using eFuse Vref\n");
+    }
+    else
+    {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
 extern "C" void app_main(void)
 {
     // Set default debug output
@@ -170,5 +218,49 @@ extern "C" void app_main(void)
     if(join())
     {
         xTaskCreate(sendMessages, "send_messages", 1024 * 4, (void *)0, 3, nullptr);
+    }
+
+    //Check if Two Point or Vref are burned into eFuse
+    check_efuse();
+
+    //Configure ADC
+    if(unit == ADC_UNIT_1)
+    {
+        adc1_config_width(ADC_WIDTH_BIT_12);
+        adc1_config_channel_atten(channel, atten);
+    }
+    else
+    {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    //Characterize ADC
+    adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    //Continuously sample ADC1
+    while(1)
+    {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for(int i = 0; i < NO_OF_SAMPLES; i++)
+        {
+            if(unit == ADC_UNIT_1)
+            {
+                adc_reading += adc1_get_raw((adc1_channel_t)channel);
+            }
+            else
+            {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
+                adc_reading += raw;
+            }
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }

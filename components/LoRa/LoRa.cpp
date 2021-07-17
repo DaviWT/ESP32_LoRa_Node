6305bdad4ee640979e1e5766859273c4884e9eb8
Davi Wei Tokikawa <davi.tokikawa@iotag.com.br>
2021-07-17 17:07:26 -0300

    Alterado para TX_MODE_4.

--- components/LoRa/LoRa.cpp
+++ components/LoRa/LoRa.cpp
@@ -177,7 +177,7 @@ static void LoRa_LmicReset()
     LMIC_setLinkCheckMode(0);
 
     // Configures LoRa TX Mode
-    LoRa_SetTxConfig(TX_MODE_3);
+    LoRa_SetTxConfig(TX_MODE_4);
 
     vTaskDelay(pdMS_TO_TICKS(100));
 }
